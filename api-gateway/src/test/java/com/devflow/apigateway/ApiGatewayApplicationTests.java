d0b4af8024432675d48dcd4fd7306c403ab7a9bc
MOHANAPRIYA0501 <mohanapriyachokkanathan@gmail.com>
2026-07-06 20:07:24 +0530

    feat: initialize API Gateway service

---
+++ api-gateway/src/test/java/com/devflow/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devflow.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
