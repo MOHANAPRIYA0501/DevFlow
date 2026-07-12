ac7a7d109a70aa85cfa73ef8dc85210f2fc99676
MOHANAPRIYA0501 <mohanapriyachokkanathan@gmail.com>
2026-07-12 15:34:29 +0530

    feat(frontend): add JWT interceptor for authenticated requests

--- devflow-frontend/src/api/axiosInstance.js
+++ devflow-frontend/src/api/axiosInstance.js
@@ -7,4 +7,20 @@ const axiosInstance = axios.create({
   },
 });
 
+// Request Interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
 export default axiosInstance;
